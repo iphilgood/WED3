e232eb2aafb7e93e65eac52fa552641ace630953
Phil Schilter <phil.schilter@gmail.com>
2017-08-22 21:34:55 +0200

    save progress of reduxstagram

---
+++ 06_React/reduxstagram/client/reducers/posts.js
@@ -0,0 +1,12 @@
+// a redurcer takes in two things:
+
+// 1. The action (info what happened)
+// 2. Copy of current state
+
+function posts(state = [], action) {
+  console.log('The post will change');
+  console.log(state, action);
+  return state;
+}
+
+export default posts;
